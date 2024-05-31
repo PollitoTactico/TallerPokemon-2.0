b5e733f1d35324bb09b974a6c998318368692d57
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-05-30 20:21:18 -0500

    Agregar archivos de proyecto.

---
+++ TallerPokemon/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TallerPokemon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TallerPokemon/Views/PokemonPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TallerPokemon.Views;
+
+public partial class PokemonPage : ContentPage
+{
+	public PokemonPage()
+	{
+		InitializeComponent();
+	}
+}
